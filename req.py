4f22ef77ea1c259b1c572f90d21c3da8ffca6040
ethiprince <131113945+ethiprince@users.noreply.github.com>
2023-04-18 16:42:36 +0530

    Update req.py

--- req.py
+++ req.py
@@ -4,15 +4,15 @@ def install_packages(*package_names):
     for package_name in package_names:
         try:
             # Check if package is installed
-            subprocess.check_call(['pip', 'freeze>
-            print(package_name + '\033[1;31m is a>
+            subprocess.check_call(['pip', 'freeze', '|', 'grep', package_name>
+            print(package_name + '\033[1;31m is already installed.\033[0m')
         except subprocess.CalledProcessError:
-            # If package is not installed, instal>
-            subprocess.check_call(['pip', 'instal>
-            print(package_name + '\033[1;31m has >
+            # If package is not installed, install it
+            subprocess.check_call(['pip', 'install', package_name])
+            print(package_name + '\033[1;31m has been installed successfully.'>
 
 # List of packages to install
-packages = ['requests', 'pyshorteners', 'instaloa>
+packages = ['requests', 'pyshorteners', 'instaloader', 'colorama']
 
 # Install packages
 install_packages(*packages)
